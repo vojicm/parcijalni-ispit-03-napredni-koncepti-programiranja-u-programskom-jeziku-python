60e1de9cd2073e105e03587769bb8f250a887df4
vojicm <vojicm@gmail.com>
2025-05-09 17:14:53 +0200

    m

---
+++ models/customer_model.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+class Customer ():
+    def __init__(self, 
+                 id, 
+                 name, 
+                 description, 
+                 price):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.price = price
+    
+    def __repr__(self):
+        return f'{self.name} ({self.id}) - {self.price}'
---
+++ models/offer_model.py
@@ -0,0 +1,6 @@
+
+
+
+
+class Offer ():
+    pass
